4d43170ca424d5d17dbfb051995aab9c7dc3cbfc
AndrewMarksArt <andrew.colin.marks@gmail.com>
2022-09-01 08:17:24 -0700

    updated hardhat.config.js to access dotenv and added a sample task to print out list of accounts

--- simple_dex/hardhat.config.js
+++ simple_dex/hardhat.config.js
@@ -1,4 +1,16 @@
+const { task } = require("hardhat/config");
+
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+// This is a sample Hardhat task
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address)
+  }
+});
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
