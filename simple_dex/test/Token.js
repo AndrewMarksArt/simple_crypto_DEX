2d650797bf36959472e764ac647ef88a968fd28b
AndrewMarksArt <andrew.colin.marks@gmail.com>
2022-09-06 09:05:43 -0700

    updated Token.sol name and added symbol, refactored test and updated for new name and symbol.

--- simple_dex/test/Token.js
+++ simple_dex/test/Token.js
@@ -2,15 +2,22 @@ const { ethers } = require('hardhat');
 const { expect } = require('chai');
 
 describe("Token", ()=> {
-    //test go inside here
-    it("has a nme", async () => {
-        // fetch token from blockchain
-        const Token = await ethers.getContractFactory('Token')
-        let token = await Token.deploy()
-        // read toekn name
-        const name = await token.name()
+    let token
+    beforeEach(async () => {
+         // fetch token from blockchain
+         const Token = await ethers.getContractFactory('Token')
+         token = await Token.deploy()
+    })
+    // check the token name is correct
+    it("has correct name", async () => {
         // check that the name is correct
-        expect(name).to.equal("My Token")
+        expect(await token.name()).to.equal("Viking Gold")
+    })
+
+    // check the token symbol is correct
+    it("has correct symbol", async () => {
+        // check that the name is correct
+        expect(await token.symbol()).to.equal("VIKINGg")
 
     })
 })
