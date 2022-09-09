f59b36387af3ee8708485e1c8969e8808837df7c
AndrewMarksArt <andrew.colin.marks@gmail.com>
2022-09-09 16:57:46 -0700

    exchange updates the token balance, can check balances, tests pass for both under tracking token deposits

--- simple_dex/contracts/Exchange.sol
+++ simple_dex/contracts/Exchange.sol
@@ -19,6 +19,7 @@ Check list of work to be completed and functionality to add
 contract Exchange {
     address public feeAccount;
     uint256 public feePercent;
+    mapping(address => mapping(address => uint256)) public tokens;
 
     constructor(address _feeAccount, uint256 _feePercent) {
         feeAccount = _feeAccount;
@@ -30,7 +31,16 @@ contract Exchange {
         // transfer tokens to exchange
         Token(_token).transferFrom(msg.sender, address(this), _amount);
         // update user balances
+        tokens[_token][msg.sender] = tokens[_token][msg.sender] + _amount;
         // emit an event
     }
+
     // Check Balances
+    function balanceOf(address _token, address _user)
+        public
+        view
+        returns (uint256)
+    {
+        return tokens[_token][_user];
+    }
 }
